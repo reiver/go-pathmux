94ec218f82ffbb2218b097e212c057229589e659
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-06-24 15:27:48 -0700

    pathmux.Mux.ServeHTTP()

---
+++ mux_servehttp.go
@@ -0,0 +1,31 @@
+package pathmux
+
+import (
+	"net/http"
+)
+
+// ServeHTTP makes &pathmux.Mux fits the http.Handler interface, and makes pathmux.Mux
+// a type of "middleware".
+func (receiver *Mux) ServeHTTP(responseWriter http.ResponseWriter, request *http.Request) {
+	if nil == receiver {
+		http.Error(responseWriter, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	url := request.URL
+	if nil == url {
+		http.Error(responseWriter, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	path := url.Path
+
+	var handler http.Handler = receiver.Handler(path)
+	if nil == handler {
+		http.NotFound(responseWriter, request)
+		return
+	}
+
+	handler.ServeHTTP(responseWriter, request)
+	return
+}
