7bce08dec05490c51f7e7661e01cd244973d0a50
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-02-29 08:33:22 -0800

    directory handlers

--- mux_handle.go
+++ mux_handle.go
@@ -1,9 +1,10 @@
 package pathmux
 
 import (
-	"github.com/reiver/go-pathmatch"
-
 	"net/http"
+
+	pkgpath "github.com/reiver/go-path"
+	"github.com/reiver/go-pathmatch"
 )
 
 // HandlePattern registers a producer (i.e., pathmux.Producer) that will create
@@ -65,6 +66,59 @@ func (receiver *Mux) HandlePath(handler http.Handler, path string) error {
 	}
 
 	receiver.pathHandlers[path] = handler
+
+	return nil
+}
+
+// HandleDirectory registers a handler (i.e., http.Handler) to handle a directory and everything under it.
+//
+// Example
+//
+//	err := mux.HandleDirectory(handler, "/wiki")
+//
+// Note that if you register "/wiki", then it will also (usually) receive requests for
+// "/wiki/", "/wiki/something", "/wiki/image.jpeg", "/wiki/once/twice/thrice/fource", etc.
+//
+// The exceptions being:
+// (1) if there is already a path-handler,
+// (2) if there is already a pattern-handler, and
+// (3) if there is a lower directory-handler.
+//
+// Also, not that HandleDirectory will remove trailing-slashes in your path.
+// So this:
+//
+//	err := mux.HandleDirectory(handler, "/wiki/")
+//
+// ... will actually be registered as:
+//
+//	err := mux.HandleDirectory(handler, "/wiki")
+func (receiver *Mux) HandleDirectory(handler http.Handler, path string) error {
+	if nil == receiver{
+		return errNilReceiver
+	}
+
+	if nil == handler {
+		return errNilHandler
+	}
+
+	path = pkgpath.Canonical("" + path)
+	path = pkgpath.RemoveTrailingSeparators(path)
+	if len(path) <= 0 || '/' != path[0] {
+		return errBadPath
+	}
+
+	receiver.mutex.Lock()
+	defer receiver.mutex.Unlock()
+
+	if nil == receiver.directoryHandlers {
+		receiver.directoryHandlers = map[string]http.Handler{}
+	}
+
+	if _, found := receiver.directoryHandlers[path]; found {
+		return errFound
+	}
+
+	receiver.directoryHandlers[path] = handler
 
 	return nil
 }
